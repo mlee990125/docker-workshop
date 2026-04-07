b7c52075ff855f2a52a77ab07e11191c09580406
mlee990125 <65577630+mlee990125@users.noreply.github.com>
2026-04-07 19:38:59 +0000

    uv + dockerizing pipelines

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,6 +7,7 @@ print(f"Running pipeline for day {day}")
 import pandas as pd
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['month'] = day 
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
