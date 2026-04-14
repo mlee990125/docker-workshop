b8fc87a2fd891aede1ef7b86cb6f8a78bb7353a9
mlee990125 <65577630+mlee990125@users.noreply.github.com>
2026-04-14 20:40:33 +0000

    ingest data and work on jupyter notebook

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,10 +1,11 @@
 import sys
+import pandas as pd
+
 print("arguments", sys.argv)
 
 day = int(sys.argv[1])
 print(f"Running pipeline for day {day}")
 
-import pandas as pd
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 df['month'] = day 
